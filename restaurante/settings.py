66e3cc68eb3256edbf40e6015860d57b5fdd0291
Marbin Alarcon <mealarcon1@misena.edu>
2022-01-26 09:43:54 -0500

    solucion de verdad

--- restaurante/settings.py
+++ restaurante/settings.py
@@ -148,10 +148,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+#STATIC_URL = 'static/'
 MEDIA_URL = '/uploads/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedMainfestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
